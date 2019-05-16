97cc818ae7150bfef30f4b79a8cccda1a08c080b
Liber Wang <yili.wang@5th.work>
2019-05-16 10:30:12 +0800

    add s3 and error-chain

--- src/ec2.rs
+++ src/ec2.rs
@@ -10,34 +10,25 @@ pub fn get_ec2_tags(instance_id: &str) -> Result<HashMap<String, String>> {
 
     let mut next_token = None;
 
+    let mut request = DescribeTagsRequest::default();
+    let mut filter = Filter::default();
+    filter.name = Some("resource-id".to_string());
+    filter.values = Some(vec![String::from(instance_id)]);
+    request.filters = Some(vec![filter]);
+
     let mut ec2_tags = HashMap::<String, String>::new();
     loop {
-        let mut request = DescribeTagsRequest::default();
-        let mut filter = Filter::default();
-        filter.name = Some("resource-id".to_string());
-        filter.values = Some(vec![String::from(instance_id)]);
-        request.filters = Some(vec![filter]);
-        request.next_token = next_token.clone();
-        let result = client.describe_tags(request).sync();
-        match result {
-            Ok(rsp) => {
-                next_token = rsp.next_token;
-                let tags = rsp.tags.unwrap();
-                for tag in tags {
-                    let key = tag.key.unwrap();
-                    let value = tag.value.unwrap();
-                    ec2_tags.insert(key, value);
-
-                }
-            }
-
-            Err(err) => {
-                warn!("error: {}", err);
-            }
+        request.next_token = next_token;
+        let rsp = client.describe_tags(request.clone()).sync()?;
+        let tags = rsp.tags.unwrap();
+        for tag in tags {
+            let key = tag.key.unwrap();
+            let value = tag.value.unwrap();
+            ec2_tags.insert(key, value);
         }
-
+        next_token = rsp.next_token;
         if next_token.is_none() {
-            return Ok(ec2_tags);
+            return Ok(ec2_tags)
         }
     }
 }
--- src/lib.rs
+++ src/lib.rs
@@ -4,6 +4,10 @@
 extern crate rusoto_credential;
 extern crate rusoto_core;
 extern crate rusoto_ec2;
+extern crate rusoto_s3;
+
+extern crate futures;
 
 pub mod ec2;
+pub mod s3;
 mod error;
